02e1bb5bb0525e3f087b4b12330d465579870e0e
Khoa Nguyen <khoanguyen793@gmail.com>
2016-02-23 09:11:25 +0000

    Add script for APIs

---
+++ Script/1_createCluster.js
@@ -0,0 +1,16 @@
+var watson = require('watson-developer-cloud');
+var retrieve_and_rank = watson.retrieve_and_rank({
+  username: '783c5984-daa9-4334-93d6-ac6f87c0b4d7',
+  password: 'uegcY7FvU5gz',
+  version: 'v1'
+});
+
+retrieve_and_rank.createCluster({
+  cluster_name: 'yeast cluster'
+},
+  function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+});
